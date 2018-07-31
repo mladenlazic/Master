935124752cf6546d4719ef111202bf39ea3b32fd
Mladen Lazic <Mladen.Lazic@zenterio.com>
2018-07-31 17:39:12 +0200

    Changed delivery location form. Corrected some names.

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,17 @@
-class Position { 
-    constructor(location_kind, location_name, lat, lng) {
-        this.location_kind = location_kind;
+// Delivery location array
+var g_DeliveryLocations = [];
+
+// Vehicle array
+var g_Vehicles = [];
+
+// Delivery Locations Markers
+var g_DeliveryLocattionsMarkers = [];
+
+class Location { 
+    constructor(location_name, lat, lng) {
         this.location_name = location_name;
         this.lat = lat;
         this.lng = lng;
-    }
-
-    getLocationKind() {
-        return this.location_kind;
     }
 
     getLocationName() {
@@ -35,6 +39,7 @@ class Position {
             icon: marker_color
         });
 
+        g_DeliveryLocattionsMarkers.push(marker);
         marker.addListener('click', function() {
             infowindow.open(map, marker);
         });
@@ -61,12 +66,6 @@ class Vehicles {
     }
 }
 
-// Location array
-var g_Positions = [];
-
-// Vehicle array
-var g_Vehicles = [];
-
 var app = angular.module('vrp', ['ngRoute']);
 app.config(function($routeProvider)
 {
@@ -79,62 +78,53 @@ app.config(function($routeProvider)
 
 app.controller("indexController", function($location, $scope)
 {
-    function hiddenInfoListItem() 
+    function hiddenLocationInfoList() 
     {
         for (i = 0; i < 10; i++) {
-            var id = "vinfoitem" + (i + 1);
+            var id = "locationInfoItem" + (i + 1);
             document.getElementById(id).style.visibility = "hidden";
         }
     }
 
-    hiddenInfoListItem();
+    hiddenLocationInfoList();
     var locationKind;
     var searchBoxPosition;
     
-    var map = new google.maps.Map(document.getElementById('gmapMap'), {
-        zoom: 8,
+    var map = new google.maps.Map(document.getElementById('googleMapContent'), {
+        zoom: 5,
         center: {lat: 44.787197, lng: 20.457273}
     });
     
     //HARDCORDED VALUE FOR TESTING
-    // g_Positions[g_Positions.length] = new Position(1, "Belgrade, Serbia", 44.786568, 20.44892159999995);
-    // g_Positions[g_Positions.length] = new Position(0, "Novi Sad, Serbia", 45.2671352, 19.83354959999997);
-    var gmapTitleLine = 40;
-	document.getElementById("insertDataMessage").style.padding = "50% 0";
-    document.getElementById("addPositions").style.display = "none";
-    document.getElementById("addVehiclePosition").style.display = "none";
-	document.getElementById("mainLayout").style.height = window.innerHeight;
-	var mainLayout = document.getElementById('mainLayout').offsetHeight;
-	var gmapRowNav = document.getElementById('gmapRowNav').offsetHeight ;
-	var gmapRowBtnCompute = document.getElementById('gmapRowBtnCompute').offsetHeight ;
-	gmapInputDataHeight = mainLayout-gmapTitleLine-gmapRowBtnCompute-gmapRowNav;
-	document.getElementById("gmapMap").style.height = mainLayout-gmapTitleLine;
-	document.getElementById("gmapInputData").style.height = gmapInputDataHeight;
-	var mainLayout = document.getElementById('insertDataMessage').offsetHeight;
-	document.getElementById("gmapInputData").style.border = "1px white solid";
+    // g_DeliveryLocations[g_DeliveryLocations.length] = new Location(1, "Belgrade, Serbia", 44.786568, 20.44892159999995);
+    // g_DeliveryLocations[g_DeliveryLocations.length] = new Location(0, "Novi Sad, Serbia", 45.2671352, 19.83354959999997);
+    var mainTitleHeight = 40;
+    document.getElementById("addLocation").style.display = "none";
+    document.getElementById("addDepotVehiclePosition").style.display = "none";
+	document.getElementById("mainForm").style.height = window.innerHeight;
+	var mainFormHeight = document.getElementById('mainForm').offsetHeight;
+	var menuNavHeight = document.getElementById('menuNav').offsetHeight;
+	mainInputFormHeight = mainFormHeight - mainTitleHeight - menuNavHeight - 12;
+	document.getElementById("googleMapContent").style.height = mainFormHeight-mainTitleHeight;
+	document.getElementById("mainInputForm").style.height = mainInputFormHeight ;
+	document.getElementById("mainInputForm").style.border = "1px white solid";
+    document.getElementById("nodata").style.marginTop = mainInputFormHeight / 2 - 80;
+
 
     function setUpStartEndPositionDesignSettings() {
-        document.getElementById("addVehiclePosition").style.display = "none";
-    	document.getElementById("addPositions").style.display = "block";
-		document.getElementById("insertDataMessage").style.display = "none";
-		var inputPositionsBox = document.getElementById('txtInsertPosition');
+        document.getElementById("nodata").style.display = "none";
+        document.getElementById("addDepotVehiclePosition").style.display = "none";
+    	document.getElementById("addLocation").style.display = "block";
+		var inputPositionsBox = document.getElementById('locationInsertName');
 		searchBoxPosition = new google.maps.places.SearchBox(inputPositionsBox);
-		var gmapRowInputText = document.getElementById('gmapRowInputText').offsetHeight;
-		var gmapRowBtnAdd = document.getElementById('gmapRowBtnAdd').offsetHeight ;
-		document.getElementById("txtaAddPositions").style.height = gmapInputDataHeight - gmapRowInputText - gmapRowBtnAdd;
-		document.getElementById("gmapInputData").style.border = "none";
+		var locationInputText = document.getElementById('locationInputText').offsetHeight;
+		var locationAddButton = document.getElementById('locationAddButton').offsetHeight ;
+		document.getElementById("locationInfoList").style.height = mainInputFormHeight - locationInputText - locationAddButton;
+		document.getElementById("mainInputForm").style.border = "none";
 		inputPositionsBox.value="";
     }
 
     function setUpAddVehiclesPositionDesignSettings() {
-        document.getElementById("addVehiclePosition").style.display = "block";
-        document.getElementById("addPositions").style.display = "none";
-        document.getElementById("insertDataMessage").style.display = "none";
-        document.getElementById("gmapInputData").style.border = "none";
-        var selectPossibleVehiclePosition = document.getElementById('selectPossibleVehiclePosition').offsetHeight;
-        var btnAddVehicleStartPosition = document.getElementById('btnAddVehicleStartPosition').offsetHeight;
-        document.getElementById("txtaAddVehicles").style.height = gmapInputDataHeight - selectPossibleVehiclePosition - btnAddVehicleStartPosition;
-
     }
 
     function calculateAndDisplayRoute(directionsService, directionsDisplay) {
@@ -155,8 +145,8 @@ app.controller("indexController", function($location, $scope)
     function getAllLocationName() {
         var p = [];
 
-        for (i = 0; i < g_Positions.length; i++) {
-            p.push(g_Positions[i].getLocationName());
+        for (i = 0; i < g_DeliveryLocations.length; i++) {
+            p.push(g_DeliveryLocations[i].getLocationName());
         }
 
         return p;
@@ -166,121 +156,90 @@ app.controller("indexController", function($location, $scope)
     {
         console.log("Enter in updateLocationInfoList");
         //read location from the class and add to the info list. The list always reflesh after some change.
-
-    }
-
-    function updateVehicleInfoList()
-    {
-        console.log("Enter in updateVehicleInfoList");
-        for (i = 0; i < g_Vehicles.length; i++) {
-            var currentItem = "item" + (i  + 1);
-            var currentItemDiv = "vinfoitem" + (i + 1);
-            document.getElementById(currentItem).innerHTML = g_Vehicles[i].getVehicleName();
+        for (i = 0; i < g_DeliveryLocations.length; i++) {
+            var currentItem = "item" + (i + 1);
+            var currentItemDiv = "locationInfoItem" + (i + 1);
+            document.getElementById(currentItem).innerHTML = g_DeliveryLocations[i].getLocationName();
             document.getElementById(currentItemDiv).style.visibility = "visible";
 
         }
+
     }
 
+    // Delete marker from the array
+    function deleteMapMarker(index) {
+        g_DeliveryLocattionsMarkers[index].setMap(null);
+    }
+
+    // Delete selected location from the array
     $scope.deleteItem = function(index) {
-        g_Vehicles.splice(index, 1);
-        hiddenInfoListItem();
-        updateVehicleInfoList();
-
+        deleteMapMarker(index);
+        g_DeliveryLocattionsMarkers.splice(index, 1);
+        g_DeliveryLocations.splice(index, 1);
+        hiddenLocationInfoList();
+        updateLocationInfoList();
     }
 
+    // Reload whole page
 	$scope.reloadPage = function() {
-        
 		location.reload();
 	}
 
-	$scope.addStartLocation = function() {
+    // Set main settings and add placeholder to the input text
+	$scope.addDeliveryLocation = function() {
 		setUpStartEndPositionDesignSettings();
-		document.getElementById("txtInsertPosition").placeholder = "Enter start position...";
-		locationKind = 1;
+		document.getElementById("locationInsertName").placeholder = "Enter location name...";
 	}
 
-	$scope.addEndLocation = function() {
-		setUpStartEndPositionDesignSettings();
-		document.getElementById("txtInsertPosition").placeholder = "Enter end position...";
-		locationKind = 0;
-	}
-
-	$scope.addStartEndLocation = function() {
+    // Add delivery location to the global array g_DeliveryLocations
+	$scope.addDeliveryLocationToArray = function() {
 		var places = searchBoxPosition.getPlaces();
         
         var location_name = places[0].formatted_address;
-		var location_kind = locationKind
         var lat = places[0].geometry.location.lat();
 		var lng = places[0].geometry.location.lng();
 
-        g_Positions[g_Positions.length] = new Position(location_kind, location_name, lat, lng);
+        g_DeliveryLocations[g_DeliveryLocations.length] = new Location(location_name, lat, lng);
 
-        if (location_kind) {
-			var marker_color = "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-		}
-		else {
-			var marker_color = "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
-		}
-		g_Positions[g_Positions.length-1].addMapMarker(map, marker_color);
-		document.getElementById('txtInsertPosition').value="";
+        var marker_color = "http://maps.google.com/mapfiles/ms/icons/red-dot.png";
+
+		g_DeliveryLocations[g_DeliveryLocations.length-1].addMapMarker(map, marker_color);
+		document.getElementById('locationInsertName').value="";
         updateLocationInfoList();
 	}
 
-    $scope.addVehicle = function() {
 
-        var name = "vehicle_" + g_Vehicles.length;
-        var location = document.getElementById("selectVehicleLocation").value;
-        if (!(location == "")) {
-            g_Vehicles[g_Vehicles.length] = new Vehicles(g_Vehicles.length, name, location);
-        }
-
-        updateVehicleInfoList();
-
-    }
-
-	$scope.addVehiclesPosition = function() {
+	$scope.addDepotVehicle = function() {
         setUpAddVehiclesPositionDesignSettings();
-
-        $('#selectVehicleLocation').children().remove().end().append('<option value="" selected disabled hidden>Choose here</option>');
-        var locations = getAllLocationName();
-        for (i = 0; i < locations.length; i++) {
-            $("#selectVehicleLocation").append('<option value=\"'+locations[i]+'\">'+locations[i]+'</option>');
-        }
 	}
 
 	$scope.computeRoute = function() {
 
-        var startPosition = [];
-        var endPosition = [];
+        var deliveryLocations = [];
 
-        for(var i = 0; i < g_Positions.length; i++)
+        for(var i = 0; i < g_DeliveryLocations.length; i++)
         {
-            console.log(g_Positions[i].getLocationKind() + g_Positions[i].getLocationName() + g_Positions[i].getLatitude() + g_Positions[i].getLongitude());
-            var obj = {"lat": g_Positions[i].getLatitude(), "lng": g_Positions[i].getLongitude()};
-            if (g_Positions[i].getLocationKind() == 1) {
-                startPosition.push(obj);
-            }
-            else {
-                endPosition.push(obj);
-            }
+            console.log(g_DeliveryLocations[i].getLocationName() + g_DeliveryLocations[i].getLatitude() + g_DeliveryLocations[i].getLongitude());
+            var obj = {"lat": g_DeliveryLocations[i].getLatitude(), "lng": g_DeliveryLocations[i].getLongitude()};
+            deliveryLocations.push(obj);
         }
 
-        var strVehicles = "{\"vehicles\":[";
-        for (i = 0; i < g_Vehicles.length; i++) {
-            var id = "\"id\":" + "\"" + g_Vehicles[i].getVehicleId() + "\"";
-            var location = "\"location\":" + "\"" + g_Vehicles[i].getVehicleLocation() + "\"";
-            strVehicles += "{" + id + "," + location + "}";
-            if (i < (g_Vehicles.length-1)) {
-                strVehicles += ",";
-            }
-        }
-        strVehicles += "]}";
-        console.log(strVehicles);
+        // var strVehicles = "{\"vehicles\":[";
+        // for (i = 0; i < g_Vehicles.length; i++) {
+        //     var id = "\"id\":" + "\"" + g_Vehicles[i].getVehicleId() + "\"";
+        //     var location = "\"location\":" + "\"" + g_Vehicles[i].getVehicleLocation() + "\"";
+        //     strVehicles += "{" + id + "," + location + "}";
+        //     if (i < (g_Vehicles.length-1)) {
+        //         strVehicles += ",";
+        //     }
+        // }
+        // strVehicles += "]}";
+        // console.log(strVehicles);
 
         var service = new google.maps.DistanceMatrixService;
         service.getDistanceMatrix({
-            origins: startPosition,
-            destinations: endPosition,
+            origins: deliveryLocations,
+            destinations: deliveryLocations,
             travelMode: 'DRIVING',
             unitSystem: google.maps.UnitSystem.METRIC,
         },function(gmResponse, gmStatus) {
@@ -291,11 +250,13 @@ app.controller("indexController", function($location, $scope)
                 var xmlhttp = new XMLHttpRequest();
                 xmlhttp.onreadystatechange = function() {
                     if (this.readyState == 4 && this.status == 200) {
+                        // Response from vrp server
                         console.log(this.responseText);
                     }
                 }
+                // gmResponse from google map api server
                 console.log(gmResponse);
-                xmlhttp.open("POST", "server/main.php?l=" + JSON.stringify(gmResponse)+"&v=" + strVehicles,  true);
+                xmlhttp.open("POST", "server/main.php?l=" + JSON.stringify(gmResponse),  true);
                 xmlhttp.send();
             }
         });
--- server/main.php
+++ server/main.php
@@ -1,8 +1,8 @@
 <?php
 	$locations = $_REQUEST["l"];
-	$vehicles = $_REQUEST["v"];
+	
     
   	$json_locations = json_decode($locations);  	
-  	$json_vehicles = json_decode($vehicles);
-    echo $json_vehicles->{"vehicles"}[0]->{"id"};
+
+    //echo $json_vehicles->{"vehicles"}[0]->{"id"};
 ?>
